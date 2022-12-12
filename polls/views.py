1d08cc9d8c70a42fdee47a4fb9493293a30c5f82
Astatsu <vinijao123690@gmail.com>
2022-12-12 09:32:37 -0300

    Criação de Views para Perguntas

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.http import Http404
 
 def index(request):
-    return HttpResponse("Hello, There!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("QUestão Inexistente.")
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "Você está visualizando os resultados da questão %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Você está votando na questão %s." % question_id)
+
